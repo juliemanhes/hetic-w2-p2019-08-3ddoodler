fa34388a2d2da3a2d4c13f212e480933fed1ee8c
Julie Manhes <julie.manhes@hetic.net>
2018-02-06 18:44:16 +0100

    fix nav

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,14 @@
 // burger nav //
-var burger = document.querySelectorAll('.header__nav--burger div');
+var burger = document.querySelector('.header__nav--burger');
 var nav = document.querySelector('.header__nav');
 var burger1 = document.querySelector('.burger-1');
 var burger2 = document.querySelector('.burger-2');
 var burger3 = document.querySelector('.burger-3');
 
-for (var i =0; i < burger.length; i++) {
-	burger[i].addEventListener('click', function() {
-		nav.classList.toggle('hidden-nav');
-		burger1.classList.toggle('burger-transform-1');
-		burger2.classList.toggle('hidden');
-		burger3.classList.toggle('burger-transform-2');
+burger.addEventListener('click', function() {
+	nav.classList.toggle('hidden-nav');
+	burger1.classList.toggle('burger-transform-1');
+	burger2.classList.toggle('hidden');
+	burger3.classList.toggle('burger-transform-2');
 
-	});
-}
-
+});
